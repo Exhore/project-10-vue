14d22dd11cff4ec9fc54ee967e801d37c8170819
Exhore <nelsonrivca@gmail.com>
2024-02-18 22:42:46 +0000

    18-02

--- GameShop/gameshop/src/components/LogIn.vue
+++ GameShop/gameshop/src/components/LogIn.vue
@@ -10,7 +10,7 @@
         class="hs-overlay hs-overlay-backdrop-open:bg-blue-950/90 hidden size-full fixed top-0 start-0 z-80 overflow-x-hidden overflow-y-auto pointer-events-none [--overlay-backdrop:static]"
         data-hs-overlay-keyboard="false">
         <div
-            class="mt-12 hs-overlay-open: hs-overlay-open:opacity-100 hs-overlay-open:duration-500 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+            class="mt-12 pt-12 hs-overlay-open: hs-overlay-open:opacity-100 hs-overlay-open:duration-500 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
             <div
                 class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
                 <div class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700">
